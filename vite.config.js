4510f856bafda155c93caba04b9e81437dde27eb
Aleksander <Khalaimaleksandr@gmail.com>
2025-03-19 15:24:06 +0300

    изменил базовый route на ./

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from "path";
 
 export default defineConfig({
 	root: __dirname,
-	base: "/adwin-agency-test/",
+	base: "./",
 	build: {
 		outDir: path.resolve(__dirname, "dist"),
 		rollupOptions: {
